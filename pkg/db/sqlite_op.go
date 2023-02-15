6df5169fd6b2eafab99e2cce1a575365f5d2b594
1dayluo <mar1sadaze@qq.com>
2023-02-15 11:46:14 +0800

    Add: sqlite3...

--- pkg/db/sqlite_op.go
+++ pkg/db/sqlite_op.go
@@ -4,6 +4,7 @@ import (
 	"DomainMonitor/pkg/readconf"
 	"database/sql"
 	"fmt"
+	"time"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -13,20 +14,57 @@ var db_conn *sql.DB
 
 func InitSqlClient() {
 	db_conn, err := sql.Open("sqlite3", db_1)
-	// 设计以下三类表单：
-	// 当前监控 - 记录实时需要监控的域名
-	// 本次新增 - 记录每次计划任务捕获到的新增域名
-	// 本次删除 - 记录每次计划任务捕获到的减少域名
 	if err != nil {
 		panic(err)
 	}
 	defer db_conn.Close()
+	// 设计以下三类表单：
+	// 当前监控 - 记录实时需要监控的域名
+	// 本次新增 - 记录每次计划任务捕获到的新增域名
+	// 本次删除 - 记录每次计划任务捕获到的减少域名
+	domains_table_sql := `CREATE TABLE domains(
+		"DOMAIN"    TEXT     NOT NULL,
+		"SUBDOMAIN"      TEXT    NOT NULL,
+		"UPDATETIME"     DATE     NOT NULL,
+		"CHECKEDTIME" 	INT NOT NULL
+	 );`
+	addeddomain_table_sql := `CREATE TABLE added_domains(
+	"DOMAIN"    TEXT     NOT NULL,
+	"SUBDOMAIN"      TEXT    NOT NULL,
+	"UPDATETIME"     DATE     NOT NULL,
+	"CHECKEDTIME" 	INT NOT NULL
+	);`
+	deleteddomain_table_sql := `CREATE TABLE added_domains(
+		"DOMAIN"    TEXT     NOT NULL,
+		"SUBDOMAIN"      TEXT    NOT NULL,
+		"UPDATETIME"     DATE     NOT NULL,
+		"CHECKEDTIME" 	INT NOT NULL
+	);`
+	create_tables_sql := []string{domains_table_sql, addeddomain_table_sql, deleteddomain_table_sql}
+	for _, v := range create_tables_sql {
+		query, err := db_conn.Prepare(v)
+		if err != nil {
+			panic(err)
+		}
+		query.Exec()
+	}
+
+	defer db_conn.Close()
 }
 
-func insert_added() {
+func insert_added(domain string, subdomain string, checked_time int) {
 	//@title insert_added
-	//@param
+	//@param domain(string) subdomain(string) checked_time(int)
 	//Return
-	fmt.Println("插入数据到表：当前监控")
-	fmt.Println("插入数据到表：本次新增")
+	current_time := time.Now().Format("2006-01-02 15:04:05")
+	stmt, err := db_conn.Prepare("INSERT INTO domains (DOMAIN, SUBDOMAIN, UPDATETIME, CHECKEDTIME) VALUES (?, ?, ?, ?)")
+	if err != nil {
+		panic(err)
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(domain, subdomain, current_time, checked_time)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Row inserted successfully!")
 }
